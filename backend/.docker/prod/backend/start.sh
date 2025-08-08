95fe16157e640edb5dd790c9fe757e6d903a7936
wallrony <wrony.neves@gmail.com>
2025-08-08 16:34:37 -0300

    feat: add fixtures migrations on start.sh for docker prod settings

--- backend/.docker/prod/backend/start.sh
+++ backend/.docker/prod/backend/start.sh
@@ -2,7 +2,8 @@
 
 service nginx restart
 
-poetry run python manage.py migrate
+python manage.py migrate
+python manage.py loaddata users.json properties.json tenants.json rent_contracts.json rent_payments.json expenses.json
 
 echo "Starting Django Backend"
-poetry run gunicorn pauli.wsgi:application --bind 0.0.0.0:8001 --timeout 120
+poetry run gunicorn property_manager.wsgi:application --bind 0.0.0.0:8001 --timeout 120
